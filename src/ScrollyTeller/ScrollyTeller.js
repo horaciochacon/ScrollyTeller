77d1be09c10dc6a028d03aff2fb516443c0fb6a5
Ryan Shackleton <ryshack@uw.edu>
2020-01-21 13:55:23 -0800

    Remove 'graphic' parameter that is passed to Scrollama, but never used.

--- src/ScrollyTeller/ScrollyTeller.js
+++ src/ScrollyTeller/ScrollyTeller.js
@@ -348,7 +348,6 @@ export default class ScrollyTeller {
       sectionConfig.scroller
         .setup({
           step: `#${sectionId} .${css.narrationBlock}`,
-          graphic: `#${graphContainerId}`,
           offset: TRIGGER_OFFSET,
           progress: true,
         })
